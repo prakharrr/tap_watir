526d4c5317cd08bf77d35985bf549fed30207ec8
Prakhar Rawat <pxr7185@g.rit.edu>
2018-07-30 11:33:40 -0700

    contains exist and its spec

--- lib/tap_watir/element.rb
+++ lib/tap_watir/element.rb
@@ -1,19 +1,28 @@
 require 'watir'
-
 module TapWatir
   class Element
-
     def initialize(driver,selector)
       @driver = driver
       @selector = selector
     end
-    private
     def locate
       @element = @driver.find_element(@selector.keys.first,@selector.values.first)
     end
-
+    def exists?
+      return false if @element
+      assert_exists
+      true
+    rescue Watir::WebDriverError::Element::NoSuchElementError
+      false
+    end
+    def assert_exists
+      locate unless @element
+      return if @element
+      raise UnknownObjectException
+    end
     def wd
       @element || locate
     end
+    private :locate, :assert_exists
   end
 end
